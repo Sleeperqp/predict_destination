80a5168d87d7072030be9274698b536780063b11
Sleeperqp <qipeng1992@gmail.com>
2015-11-20 00:45:13 +0800

    加入meanshift聚类方法

--- src/s_kmeans.py
+++ src/s_kmeans.py
@@ -1,13 +1,49 @@
 #coding=utf-8
 __author__ = 'sleeper'
 
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, MeanShift, estimate_bandwidth
 
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import json
+from geoplotlib.colors import create_set_cmap
+import pyglet
 import geoplotlib
+from geoplotlib.layers import BaseLayer
+from geoplotlib.core import BatchPainter
+from geoplotlib.utils import BoundingBox
+
+
+class KMeansLayer(BaseLayer):
+
+    def __init__(self, data):
+        self.data = data
+
+
+    def invalidate(self, proj):
+        self.painter = BatchPainter()
+        x, y = proj.lonlat_to_screen(self.data['lon'], self.data['lat'])
+
+        k_means = KMeans()
+        k_means.fit(np.vstack([x,y]).T)
+        labels = k_means.labels_
+
+        self.cmap = create_set_cmap(set(labels), 'hsv')
+        for l in set(labels):
+            try:
+                self.painter.set_color(self.cmap[l])
+                self.painter.convexhull(x[labels == l], y[labels == l])
+                self.painter.points(x[labels == l], y[labels == l], 2)
+            except Exception:
+                print '=============',l,'=============='
+
+    def draw(self, proj, mouse_x, mouse_y, ui_manager):
+        self.painter.batch_draw()
+
+
+    def on_key_release(self, key, modifiers):
+        return False
 
 def read_csvfile(filepath):
     datas = pd.read_csv(filepath)
@@ -72,13 +108,42 @@ def s_kmeans(filepath):
     #print tor.labels_
     draw_kmeans(tor, datas)
 
+def s_meanshif(filepath):
+    datas = read_csvfile(filepath)
+    datas = np.array(datas)
+    # The following bandwidth can be automatically detected using
+    bandwidth = estimate_bandwidth(datas, quantile=0.2, n_samples=100000)
+
+    ms = MeanShift(bandwidth=bandwidth, bin_seeding=True, min_bin_freq=5)
+    ms.fit(datas)
+    labels = ms.labels_
+    cluster_centers = ms.cluster_centers_
+
+    labels_unique = np.unique(labels)
+    n_clusters_ = len(labels_unique)
+
+    geo_data = {'lat':[],'lon':[]}
+    for xy in cluster_centers:
+        x, y = xy[0],xy[1]
+        geo_data['lon'].append(x)
+        geo_data['lat'].append(y)
+
+    #print geo_data
+    geoplotlib.dot(geo_data)
+    geoplotlib.show()
+
+    print("number of estimated clusters : %d" % n_clusters_)
+
+
 def draw_geo():
-    thedata = geoplotlib.utils.read_csv('../data/des.csv')
-    geoplotlib.dot(thedata)
+    data = geoplotlib.utils.read_csv('../data/des.csv')
+    geoplotlib.add_layer(KMeansLayer(data))
+    geoplotlib.set_smoothing(True)
     geoplotlib.show()
 
 
 if __name__=="__main__":
     #datas = read_csvfile("../data/train.csv")
-    draw_geo()
+    #draw_geo()
     #s_kmeans("../data/train.csv")
+    s_meanshif("../data/train.csv")
